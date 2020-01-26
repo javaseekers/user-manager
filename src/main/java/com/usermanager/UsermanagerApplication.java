0906afe3efd7070350c08256cbe80144dd652349
javaseekers <javaseekers9@gmail.com>
2020-01-26 11:56:31 +0530

    initial version

---
+++ src/main/java/com/usermanager/UsermanagerApplication.java
@@ -0,0 +1,15 @@
+package com.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagerApplication.class, args);
+		
+		System.out.println("ssss");
+	}
+
+}
